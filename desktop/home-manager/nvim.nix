5ef592a85e5cbb3bf34f3fd8031d46c6b32e8253
hexolexo <hexolexo132@proton.me>
2025-12-15 12:32:15 +1100

    desktop: gen 208

--- desktop/home-manager/nvim.nix
+++ desktop/home-manager/nvim.nix
@@ -1,6 +1,7 @@
 {...}: {
   programs.nixvim = {
     enable = true;
+    clipboard.providers.wl-copy.enable = true;
     globals.mapleader = " ";
     globals.maplocalleader = " ";
     colorschemes.catppuccin = {
@@ -52,35 +53,36 @@
       }
     ];
     extraConfigLua = ''
-      vim.o.timeoutlen = 500
-                local builtin = require('telescope.builtin')
+      vim.opt.clipboard = 'unnamedplus'
+        vim.o.timeoutlen = 500
+                  local builtin = require('telescope.builtin')
 
-            vim.keymap.set('n', '<leader>ps', function()
-              builtin.grep_string({ search = vim.fn.input('Grep > ') })
-            end, { desc = '[P]roject [S]earch' })
-              local keymap = vim.keymap.set
-              keymap('n', 'n', 'h', { noremap = true })
-              keymap('n', 'e', 'k', { noremap = true })
-              keymap('n', 'i', 'j', { noremap = true })
-              keymap('n', 'o', 'l', { noremap = true })
-              keymap('n', 'h', 'b', { noremap = true })
-              keymap('n', "'", 'e', { noremap = true })
-              keymap('n', ',', 'o', { noremap = true })
+              vim.keymap.set('n', '<leader>ps', function()
+                builtin.grep_string({ search = vim.fn.input('Grep > ') })
+              end, { desc = '[P]roject [S]earch' })
+                local keymap = vim.keymap.set
+                keymap('n', 'n', 'h', { noremap = true })
+                keymap('n', 'e', 'k', { noremap = true })
+                keymap('n', 'i', 'j', { noremap = true })
+                keymap('n', 'o', 'l', { noremap = true })
+                keymap('n', 'h', 'b', { noremap = true })
+                keymap('n', "'", 'e', { noremap = true })
+                keymap('n', ',', 'o', { noremap = true })
 
-              keymap('v', 'n', 'h', { noremap = true })
-              keymap('v', 'e', 'k', { noremap = true })
-              keymap('v', 'i', 'j', { noremap = true }) -- Error is happening here
-              keymap('v', 'o', 'l', { noremap = true })
-              keymap('v', 'h', 'b', { noremap = true })
-              keymap('v', "'", 'e', { noremap = true })
-              keymap('v', ',', 'o', { noremap = true })
+                keymap('v', 'n', 'h', { noremap = true })
+                keymap('v', 'e', 'k', { noremap = true })
+                keymap('v', 'i', 'j', { noremap = true }) -- Error is happening here
+                keymap('v', 'o', 'l', { noremap = true })
+                keymap('v', 'h', 'b', { noremap = true })
+                keymap('v', "'", 'e', { noremap = true })
+                keymap('v', ',', 'o', { noremap = true })
 
-              keymap('n', ';', 'p', { noremap = true })
-              keymap('n', 'u', 'i', { noremap = true })
-              keymap('n', 'l', 'u', { noremap = true })
+                keymap('n', ';', 'p', { noremap = true })
+                keymap('n', 'u', 'i', { noremap = true })
+                keymap('n', 'l', 'u', { noremap = true })
 
-              -- Include uppercase
-              keymap('n', 'U', 'I', { noremap = true })
+                -- Include uppercase
+                keymap('n', 'U', 'I', { noremap = true })
     '';
   };
 }
