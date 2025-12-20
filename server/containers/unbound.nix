bccd1a368444126594a3651af7c9ee669ecf40b3
hexolexo <hexolexo132@proton.me>
2025-12-20 13:34:13 +1100

    vault deployed

---
+++ server/containers/unbound.nix
@@ -0,0 +1,33 @@
+{...}: {
+  containers.fuzzing = {
+    #
+    autoStart = true;
+    privateNetwork = false;
+    config = {
+      pkgs,
+      lib,
+      ...
+    }: {
+      services.unbound = {
+        enable = true;
+        settings = {
+          server = {
+            interface = ["10.0.0.1" "127.0.0.1" "::1"];
+            access-control = ["127.0.0.0/8 allow" "::1 allow"];
+
+            auto-trust-anchor-file = "/var/lib/unbound/root.key";
+
+            hide-identity = true;
+            hide-version = true;
+
+            prefetch = true;
+            num-threads = 2;
+          };
+        };
+      };
+      boot.isContainer = true;
+
+      system.stateVersion = "25.05";
+    };
+  };
+}
