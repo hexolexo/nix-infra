f1f7038920e4633fad30e0606a6b5ec1b74fb76c
hexolexo <hexolexo132@proton.me>
2025-12-22 13:28:51 +1100

    vault deployed

--- server/configuration.nix
+++ server/configuration.nix
@@ -10,7 +10,7 @@ in {
     ./services/virtualisation.nix
     # Optional Services: #
     #./services/paperless-ngx.nix
-    ./containers/unbound.nix
+    #./containers/unbound.nix
     #./containers/murmur.nix
     #./services/apt-cacher-ng.nix
     ./services/minecraft.nix
