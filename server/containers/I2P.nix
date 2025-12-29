535e02cc7e9150ac641d177dd55a39db8a8c30de
hexolexo <hexolexo132@proton.me>
2025-12-29 20:21:06 +1100

    vault deployed

--- server/containers/I2P.nix
+++ server/containers/I2P.nix
@@ -26,4 +26,9 @@
       system.stateVersion = "25.11"; # If you don't add a state version, nix will complain at every rebuild
     };
   };
+  networking.firewall.allowedTCPPorts = [
+    7070 # default web interface port
+    4447 # default socks proxy port
+    4444 # default http proxy port
+  ];
 }
