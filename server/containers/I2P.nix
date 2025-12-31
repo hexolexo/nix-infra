45b23382376626e3b2712ca0bcc839aeb40d50ea
hexolexo <hexolexo132@proton.me>
2025-12-31 14:01:37 +1100

    vault deployed

--- server/containers/I2P.nix
+++ server/containers/I2P.nix
@@ -1,35 +1,40 @@
 {
   pkgs,
+  lib,
+  config,
   secrets,
   ...
 }: {
-  containers.i2pd-container = {
-    autoStart = true;
-    privateNetwork = true;
-    hostAddress = "192.168.152.10";
-    localAddress = "192.168.152.11";
-    config = {...}: {
-      networking = {
-        defaultGateway = "192.168.152.10";
-        nameservers = ["192.168.100.1"]; # libvirt dnsmasq
-        # Exposing the nessecary ports in order to interact with i2p from outside the container
-        firewall.allowedTCPPorts = [
-          7070 # default web interface port
-          4447 # default socks proxy port
-          4444 # default http proxy port
-        ];
-      };
-
-      services.i2pd = {
-        enable = true;
-        address = "192.168.152.11";
-        proto = {
-          http.enable = true;
-          socksProxy.enable = true;
-          httpProxy.enable = true;
+  options.services.i2pd-container.enable = lib.mkEnableOption "i2pd container";
+  config = lib.mkIf config.services.i2pd-container.enable {
+    containers.i2pd-container = {
+      autoStart = true;
+      privateNetwork = true;
+      hostAddress = "192.168.152.10";
+      localAddress = "192.168.152.11";
+      config = {...}: {
+        networking = {
+          defaultGateway = "192.168.152.10";
+          nameservers = ["192.168.100.1"]; # libvirt dnsmasq
+          # Exposing the nessecary ports in order to interact with i2p from outside the container
+          firewall.allowedTCPPorts = [
+            7070 # default web interface port
+            4447 # default socks proxy port
+            4444 # default http proxy port
+          ];
         };
+
+        services.i2pd = {
+          enable = true;
+          address = "192.168.152.11";
+          proto = {
+            http.enable = true;
+            socksProxy.enable = true;
+            httpProxy.enable = true;
+          };
+        };
+        system.stateVersion = "25.11"; # If you don't add a state version, nix will complain at every rebuild
       };
-      system.stateVersion = "25.11"; # If you don't add a state version, nix will complain at every rebuild
     };
   };
   # networking config exists in virtual-networking.nix
