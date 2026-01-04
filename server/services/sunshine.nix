ac6912a6d7f6b65b6fa855274d6413454f1a251b
hexolexo <hexolexo132@proton.me>
2026-01-04 17:21:53 +1100

    vault deployed

--- server/services/sunshine.nix
+++ server/services/sunshine.nix
@@ -1,30 +1,57 @@
 {pkgs, ...}: {
   nixpkgs.config.allowUnfree = true;
+
   services.sunshine = {
     enable = true;
     autoStart = true;
     capSysAdmin = true;
     openFirewall = true;
   };
+
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
   };
+
   programs.steam.enable = true;
-  services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false; # Server, not laptop
-    open = false; # Quadros typically need proprietary
-  };
+
   services.xserver = {
     enable = true;
-    # WARN: Check your Quadro model - some need different BusID values
+    videoDrivers = ["nvidia"];
+    displayManager = {
+      lightdm.enable = true;
+      autoLogin = {
+        enable = true;
+        user = "hexolexo";
+      };
+      defaultSession = "none+openbox";
+    };
+    windowManager.openbox.enable = true;
+
+    # HACK: Forces Nvidia to create virtual displays headless
     deviceSection = ''
       Option "AllowEmptyInitialConfiguration"
+      Option "UseDisplayDevice" "none"
+      Option "ConnectedMonitor" "DFP-0"
     '';
+
     screenSection = ''
       Option "metamodes" "1920x1080_60 +0+0"
     '';
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    open = false;
+    # nvidiaPersistenced = true; // Keeps GPU initialized without X
+  };
+
+  # Virtual audio sink for Sunshine to capture
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
   };
 }
