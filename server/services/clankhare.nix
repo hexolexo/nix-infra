3eb06104803694430ac29532ea80292fee8a7161
hexolexo <hexolexo132@proton.me>
2026-01-20 14:09:23 +1100

    vault deployed

--- server/services/clankhare.nix
+++ server/services/clankhare.nix
@@ -15,6 +15,7 @@
     description = "Clankhare Discord Bot";
     after = ["network-online.target"];
     wantedBy = ["multi-user.target"];
+    wants = ["network-online.target"];
 
     serviceConfig = {
       ExecStart = "${inputs.clankhare.packages.${pkgs.system}.default}/bin/Clankhare";
