fbd1efc16465b28d047ebf28cb74bfa4c7276755
hexolexo <hexolexo132@proton.me>
2026-01-26 17:12:30 +1100

    vault deployed

--- server/configuration.nix
+++ server/configuration.nix
@@ -18,7 +18,7 @@ in {
     #./services/minecraft.nix
     #./containers/mindustry.nix
     #./containers/terraria.nix      #  WARN: Untested
-    ./containers/I2P.nix
+    #./containers/I2P.nix
     #./containers/jellyfin.nix
     #./containers/fuzzing.nix       #  NOTE: I'll probably want to start using this at some point
     #./containers/monitoring.nix    #  NOTE: Functional but overkill for this project
