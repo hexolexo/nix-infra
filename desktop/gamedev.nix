e28e79565107b7329b799f93e4e4d43ff3e085ec
hexolexo <hexolexo132@proton.me>
2026-02-12 13:49:21 +1100

    desktop: gen 320

--- desktop/gamedev.nix
+++ desktop/gamedev.nix
@@ -2,6 +2,7 @@
   nixpkgs.config.android_sdk.accept_license = true;
   environment.systemPackages = with pkgs; [
     godotPackages_4_4.godot
+    pkgsRocm.blender
     android-studio
     android-tools
     androidenv.androidPkgs.androidsdk
