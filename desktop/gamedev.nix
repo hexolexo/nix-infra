55059113965e4cac52c148ed191881b7a9308cc4
hexolexo <hexolexo132@proton.me>
2026-02-12 13:07:54 +1100

    desktop: gen 318

--- desktop/gamedev.nix
+++ desktop/gamedev.nix
@@ -2,5 +2,11 @@
   nixpkgs.config.android_sdk.accept_license = true;
   environment.systemPackages = with pkgs; [
     godotPackages_4_3.godot
+    android-studio
+    android-tools
+    androidenv.androidPkgs.androidsdk
+    androidenv.androidPkgs.emulator
+    androidenv.androidPkgs.ndk-bundle
+    jdk
   ];
 }
