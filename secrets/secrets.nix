52a47d2f32c2075ef58e0d2eb8dbee484bf959cf
hexolexo <hexolexo132@proton.me>
2026-04-18 18:30:18 +1000

    chore: rotate vault host key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   hexolexo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBn10kNU91QinvzDnJ/d6SMivvh+732dmcbHY4YurxGM hexolexo@hexolexo";
 
-  vault = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMr/2C3aRlh0D0IBA8R/8dIxbxv7+SlDsPbUeWfYSdQu vault";
+  vault = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICoQhKRs6u3Dyf9J4qoTS324Yx1Ogei7Qswe/RQ8JSsZ vault";
 in {
   "wireguard-hub-key.age".publicKeys = [hexolexo vault];
   "wireguard-hexolexo-key.age".publicKeys = [hexolexo];
