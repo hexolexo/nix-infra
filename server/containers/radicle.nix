1e12fea565f31518846ebbd60d98a846089800ce
hexolexo <hexolexo132@proton.me>
2026-04-19 18:10:51 +1000

    vault deployed

--- server/containers/radicle.nix
+++ server/containers/radicle.nix
@@ -18,19 +18,6 @@
       isReadOnly = true;
     };
     config = {pkgs, ...}: {
-      services.nginx = {
-        enable = true;
-        virtualHosts."_" = {
-          listen = [
-            {
-              addr = "10.0.0.1";
-              port = 18080;
-            }
-          ];
-          root = "${pkgs.radicle-explorer}";
-          locations."/api".proxyPass = "http://127.0.0.1:8080";
-        };
-      };
       services.radicle = {
         enable = true;
         checkConfig = false; #  HACK: validator is broken in current nixpkgs
@@ -39,7 +26,7 @@
         node.openFirewall = true;
         node.listenAddress = "[::0]";
         httpd = {
-          enable = true;
+          enable = false;
           #listenAddress = "10.0.0.1";
         };
         settings = {
