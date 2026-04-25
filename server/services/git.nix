b795fa3cb27ab5fe440ff37fb6303e1c933cc08c
hexolexo <hexolexo132@proton.me>
2026-04-25 13:06:14 +1000

    vault deployed

--- server/services/git.nix
+++ server/services/git.nix
@@ -18,7 +18,7 @@ in {
     lfs.enable = true;
     database.type = "sqlite3";
     settings = {
-      F3.PATH = "/data/forgejo/f3";
+      F3.ENABLED = false;
       server = {
         DOMAIN = "10.0.0.1";
         ROOT_URL = "http://10.0.0.1:${toString forgejoPort}/";
