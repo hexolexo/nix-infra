2416b0aa39fbb9f5a320652a52c2bf59c25583fa
hexolexo <hexolexo132@proton.me>
2026-04-28 21:26:57 +1000

    vault deployed

--- server/services/git.nix
+++ server/services/git.nix
@@ -48,7 +48,7 @@ in {
       url = "http://10.0.0.1:${toString forgejoPort}";
       tokenFile = "/run/secrets/forgejo-runner-token";
       labels = [
-        "ubuntu-latest:docker://node:24-bookworm-slim"
+        "ubuntu-latest:docker://ghcr.io/catthehacker/ubuntu:act-22.04"
       ];
     };
   };
