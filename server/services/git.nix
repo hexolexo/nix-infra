50086f6f0003e0d693c7fe022f4e15006ceab4fd
hexolexo <hexolexo132@proton.me>
2026-04-30 18:18:45 +1000

    vault deployed

--- server/services/git.nix
+++ server/services/git.nix
@@ -18,6 +18,7 @@ in {
       server = {
         DOMAIN = "forgejo.internal";
         ROOT_URL = "http://forgejo.internal/";
+        LOCAL_ROOT_URL = "http://127.0.0.1:${toString forgejoPort}/";
         HTTP_ADDR = "10.0.0.1";
         HTTP_PORT = forgejoPort;
         START_SSH_SERVER = true;
@@ -53,9 +54,6 @@ in {
       settings = {
         container = {
           dns_server = ["10.0.0.1"];
-        };
-        runner = {
-          insecure = true;
         };
       };
     };
