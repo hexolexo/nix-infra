c19e7e5af6e4b66053ca3c9e16d41f95968442cc
hexolexo <hexolexo132@proton.me>
2026-05-02 19:16:41 +1000

    vault deployed

--- server/services/minecraft.nix
+++ server/services/minecraft.nix
@@ -18,19 +18,7 @@
         gamemode = 0;
         difficulty = 2;
         spawn-protection = 0;
-        level-type = "flat"; # BentoBox overrides world gen anyway
         online-mode = true;
-      };
-
-      symlinks = {
-        "plugins/BentoBox.jar" = pkgs.fetchurl {
-          url = "https://github.com/BentoBoxWorld/BentoBox/releases/download/2.4.1/BentoBox-2.4.1.jar";
-          sha256 = ""; # HACK: fill this in - run nix-prefetch-url on the URL to get it
-        };
-        "plugins/BSkyBlock.jar" = pkgs.fetchurl {
-          url = "https://github.com/BentoBoxWorld/BSkyBlock/releases/latest/download/BSkyBlock.jar";
-          sha256 = ""; # WARN: 'latest' in a URL will drift - pin to a specific release tag
-        };
       };
     };
   };
