07c087bb47886e7b11cfd6f59a59965b1e9686b6
hexolexo <hexolexo132@proton.me>
2026-05-12 18:55:16 +1000

    vault deployed

--- machines/vault/services/nats.nix
+++ machines/vault/services/nats.nix
@@ -8,16 +8,16 @@
             # User Key
             nkey = "UAAHP3OLG6XC6XKMNBRAWXADF4SQG6UKFFBZD2B45UW25KEN4OYFO3EM";
             permissions = {
-              publish = "libvirt.>";
-              subscribe = "_INBOX.>";
+              publish = ["libvirt.>"];
+              subscribe = ["_INBOX.>"];
             };
           }
           {
             # Libvirt listener
             nkey = "UDOLBO4NERUUR2E7V7YBCG7K7UIXUKE2ITKKKRT7FGXE5RTU4PE5NQT5";
             permissions = {
-              publish = "_INBOX.>";
-              subscribe = "libvirt.>";
+              publish = ["_INBOX.>"];
+              subscribe = ["libvirt.>"];
             };
           }
         ];
