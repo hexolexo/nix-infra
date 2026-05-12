4a6498c8614baf5f31a4cfe277f02f513526fa6f
hexolexo <hexolexo132@proton.me>
2026-05-12 18:29:31 +1000

    vault deployed

--- machines/vault/services/nats.nix
+++ machines/vault/services/nats.nix
@@ -2,6 +2,17 @@
   services.nats = {
     enable = true;
     settings = {
+      authorization = {
+        users = [
+          {
+            nkey = "UAAHP3OLG6XC6XKMNBRAWXADF4SQG6UKFFBZD2B45UW25KEN4OYFO3EM";
+            permissions = {
+              publish = "libvirt.>";
+              subscribe = "libvirt.>";
+            };
+          }
+        ];
+      };
       port = 4222;
     };
   };
