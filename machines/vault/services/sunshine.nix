d0c8245bd7c4589be34f46bedd68ae87b847b134
hexolexo <hexolexo132@proton.me>
2026-05-16 12:04:23 +1000

    vault deployed

--- machines/vault/services/sunshine.nix
+++ machines/vault/services/sunshine.nix
@@ -1,31 +1,41 @@
 {pkgs, ...}: {
-  environment.systemPackages = [pkgs.weston];
+  users.users.sunshine = {
+    isSystemUser = true;
+    group = "sunshine";
+    extraGroups = ["video" "render" "input" "tty" "seat"];
+  };
 
-  systemd.user.services.weston = {
+  users.groups.sunshine = {};
+
+  systemd.services.weston = {
     description = "Weston headless compositor";
-    wantedBy = ["default.target"];
+    wantedBy = ["multi-user.target"];
     serviceConfig = {
+      User = "sunshine";
       ExecStart = "${pkgs.weston}/bin/weston --backend=headless --width=1920 --height=1080";
       Restart = "on-failure";
+      RuntimeDirectory = "sunshine-wayland";
     };
     environment = {
-      XDG_RUNTIME_DIR = "/run/user/1000";
+      XDG_RUNTIME_DIR = "/run/sunshine-wayland";
     };
   };
 
-  systemd.user.services.sunshine = {
+  systemd.services.sunshine = {
     description = "Sunshine game stream host";
-    wantedBy = ["default.target"];
+    wantedBy = ["multi-user.target"];
     after = ["weston.service"];
     requires = ["weston.service"];
     serviceConfig = {
+      User = "sunshine";
       ExecStart = "${pkgs.sunshine}/bin/sunshine";
       Restart = "on-failure";
       AmbientCapabilities = "CAP_SYS_ADMIN";
       CapabilityBoundingSet = "CAP_SYS_ADMIN";
+      RuntimeDirectory = "sunshine-wayland";
     };
     environment = {
-      XDG_RUNTIME_DIR = "/run/user/1000";
+      XDG_RUNTIME_DIR = "/run/sunshine-wayland";
       WAYLAND_DISPLAY = "wayland-1";
     };
   };
