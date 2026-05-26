d8a36c47179d78634ffd4ec009bdf85e51396c59
hexolexo <hexolexo132@proton.me>
2026-05-26 21:47:21 +1000

    vault deployed

--- machines/vault/services/searx.nix
+++ machines/vault/services/searx.nix
@@ -1,4 +1,18 @@
-{...}: {
+{lib, ...}: {
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "open-webui"
+    ];
+  services.open-webui = {
+    enable = true;
+    port = 8080;
+    environment = {
+      OLLAMA_BASE_URL = "http://10.0.0.8:11434";
+      ENABLE_WEB_SEARCH = "True";
+      WEB_SEARCH_ENGINE = "searxng";
+      SEARXNG_QUERY_URL = "http://127.0.0.1:8088/search?q=<query>";
+    };
+  };
   services.searx = {
     enable = true;
     redisCreateLocally = true;
@@ -7,7 +21,7 @@
         secret_key = "TemporaryKeyBecauseThisIsAPrivateServer";
         limiter = false;
         bind_address = "10.0.0.1";
-        port = 8080;
+        port = 8088;
       };
       search.formats = ["html" "json"];
     };
