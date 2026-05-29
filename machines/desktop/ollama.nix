ff68f5a903c4802a594db556a7035b03bd4781ff
hexolexo <hexolexo132@proton.me>
2026-05-29 20:16:50 +1000

    desktop: gen 24

--- machines/desktop/ollama.nix
+++ machines/desktop/ollama.nix
@@ -2,6 +2,10 @@
   services.ollama = {
     enable = true;
     host = "10.0.0.8";
+    rocmOverrideGfx = "12.0.1";
+    environmentVariables = {
+      HCC_AMDGPU_TARGET = "gfx1201";
+    };
   };
   networking.firewall.interfaces."wg0".allowedTCPPorts = [11434];
 }
