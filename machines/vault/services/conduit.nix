7e89b02e26678f9aeebf33884bdfcb3bcde628ad
hexolexo <hexolexo132@proton.me>
2026-05-30 10:55:18 +1000

    vault deployed

--- machines/vault/services/conduit.nix
+++ machines/vault/services/conduit.nix
@@ -84,6 +84,6 @@
     enable = true;
     natsURL = "nats://localhost:4222";
     nkeySeedFile = config.age.secrets.conduit-nixos.path;
-    flakePath = "git+http://10.0.0.1/hexolexo/nix-infra?dir=.#vault";
+    flakePath = "git+http://10.0.0.1/hexolexo/nix-infra.git#vault";
   };
 }
