6d9e0af7b398f63e9b37b65d41db263143d7cbda
hexolexo <hexolexo132@proton.me>
2026-05-30 10:37:21 +1000

    Testing

--- machines/vault/services/conduit.nix
+++ machines/vault/services/conduit.nix
@@ -84,6 +84,6 @@
     enable = true;
     natsURL = "nats://localhost:4222";
     nkeySeedFile = config.age.secrets.conduit-nixos.path;
-    flakePath = "/etc/nixos";
+    flakePath = "git+http://10.0.0.1/hexolexo/nix-infra#vault";
   };
 }
