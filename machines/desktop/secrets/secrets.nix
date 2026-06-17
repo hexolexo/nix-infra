38a6755181f930642b29b6c1c47c8579aa3903b0
hexolexo <hexolexo132@proton.me>
2026-06-17 10:15:38 +1000

    chore: rotate hexolexo-pc host key

--- machines/desktop/secrets/secrets.nix
+++ machines/desktop/secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   hexolexo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBn10kNU91QinvzDnJ/d6SMivvh+732dmcbHY4YurxGM hexolexo@hexolexo";
-  hexolexo-pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEYyTR6Qa5NMSlPfRkKKWauEtw6ykIYpgNDuHwLC0WA3 hexolexo-pc";
+  hexolexo-pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAsyN3hjZZM3V+n87LCuuHJle5Wo236Mdkybvp+9DW3Z hexolexo-pc";
 in {
   "nixosNATS.age".publicKeys = [hexolexo hexolexo-pc];
 }
